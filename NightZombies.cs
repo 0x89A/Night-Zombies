89c9afdfe33ffe42e6b1f2f9bc62cf44ec2497fd
0x89A <57545695+0x89A@users.noreply.github.com>
2021-01-25 18:32:33 +0000

    Upload 2.0 version

--- NightZombies.cs
+++ NightZombies.cs
@@ -1,54 +1,330 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections;
-using System.Linq;
+using System.Collections.Generic;
+
+using ConVar;
+
+using Oxide.Core;
+using Oxide.Core.Configuration;
+using Oxide.Core.Plugins;
 
 using UnityEngine;
+using Random = UnityEngine.Random;
+using Terrain = UnityEngine.Terrain;
 
 using Newtonsoft.Json;
 
-using Rust;
-using Oxide.Game.Rust.Cui;
-
 namespace Oxide.Plugins
 {
-    [Info("Night Zombies", "0x89A", "0.9.0")]
-    [Description("Spawns murderers and scarecrows at night, kills them at sunrise.")]
-    class NightZombies : RustPlugin
+    [Info("Night Zombies", "0x89A", "2.0.1")]
+    [Description("Spawns zombies at night, kills them at sunrise")]
+    class NightZombies : CovalencePlugin
     {
-        private bool halloweenActive;
+        private Configuration config;
 
-        private Timer destroyTimer;
+        DynamicConfigFile dataFile;
+
+        private bool IsActive => Halloween.enabled;
+
+        private int daysSinceSpawn = 0;
+
+        List<BaseNetworkable> serverZombies;
+
+        [PluginReference] Plugin ConsoleFilter;
+
+        #region -Oxide Hooks-
+
+        void OnServerInitialized()
+        {
+            serverZombies = Facepunch.Pool.GetList<BaseNetworkable>();
+
+            TOD_Sky.Instance.Components.Time.OnSunrise += ToggleZombies;
+            TOD_Sky.Instance.Components.Time.OnSunset += ToggleZombies;
+
+            dataFile = Interface.Oxide.DataFileSystem.GetFile($"{Name}");
+            if (dataFile == null) return;
+
+            daysSinceSpawn = dataFile.ReadObject<int>();
+
+            DoDestroy();
+        }
+
+        void Unload()
+        {
+            TOD_Sky.Instance.Components.Time.OnSunrise -= ToggleZombies;
+            TOD_Sky.Instance.Components.Time.OnSunset -= ToggleZombies;
+
+            SaveData();
+
+            DoDestroy();
+
+            if (IsActive)
+            {
+                RunCommands(false);
+                DoDestroy();
+            }
+
+            if (!ConsoleFilter)
+            {
+                Application.logMessageReceived -= RemoveKillMessage;
+                Application.logMessageReceived += Facepunch.Output.LogHandler;
+            }
+
+            Facepunch.Pool.FreeList(ref serverZombies);
+        }
+
+        void Loaded()
+        {
+            if (ConsoleFilter)
+            {
+                PrintWarning("Console Filter detected disabling filtering, It is recommended to add \"killed by Generic\" to you Console Filter config");
+            }
+            else
+            {
+                Application.logMessageReceived += RemoveKillMessage;
+                Application.logMessageReceived -= Facepunch.Output.LogHandler;
+            }
+        }
+
+        private void SaveData() => dataFile.WriteObject(daysSinceSpawn);
+
+        void OnEntitySpawned(BaseNetworkable networkable)
+        {
+            if (IsZombie(networkable))
+            {
+                serverZombies.Add(networkable);
+                BaseCombatEntity combat = networkable as BaseCombatEntity;
+                if (combat)
+                {
+                    combat.SetHealth(combat.ShortPrefabName == "scarecrow" ? config.SpawnSettings.scarecrowHealth : config.SpawnSettings.murdererHealth);
+                    combat.SendNetworkUpdateImmediate();
+                }
+            }
+        }
+
+        bool CanNpcAttack(HTNPlayer npc, BaseEntity target)
+        {
+            return CanAttack(target);
+        }
+
+        bool CanNpcAttack(NPCMurderer npc, BaseEntity target)
+        {
+            return CanAttack(target);
+        }
+
+        void RemoveKillMessage(string message, string stackTrace, LogType type)
+        {
+            if (!string.IsNullOrEmpty(message) && !message.Contains("was killed by Generic"))
+            {
+                Facepunch.Output.LogHandler(message, stackTrace, type);
+            }
+        }
+
+        #endregion
+
+        private void ToggleZombies()
+        {
+            if (TOD_Sky.Instance.IsDay) daysSinceSpawn++;
+
+            if (IsActive) timer.Once(15f, () => DoDestroy());
+            else if (!CanSpawn()) return;
+
+            RunCommands(!IsActive);
+        }
+
+        private bool CanSpawn()
+        {
+            if (Random.Range(0, 101) > config.SpawnSettings.Chance.chance || 
+                daysSinceSpawn < config.SpawnSettings.Chance.days ||
+                !config.SpawnSettings.reverseTimings && TOD_Sky.Instance.IsNight || 
+                config.SpawnSettings.reverseTimings && TOD_Sky.Instance.IsDay) return false;
+
+            return true;
+        }
+
+        private bool IsZombie(BaseNetworkable networkable)
+        {
+            return (networkable is NPCMurderer || networkable is HTNPlayer) && !(networkable is Scientist || networkable is ScientistNPC || networkable.ShortPrefabName.Contains("scientist"));
+        }
+
+        #region -Util-
+
+        private bool CanAttack(BaseEntity target)
+        {
+            if (config.BehaviourSettings.ifContains)
+            {
+                for (int i = 0; i < config.BehaviourSettings.ignored.Count; i++)
+                    if (target.ShortPrefabName.Contains(config.BehaviourSettings.ignored[i])) return false;
+            }
+
+            return config.BehaviourSettings.ignored.Contains(target.ShortPrefabName);
+        }
+
+        private void RunCommands(bool on)
+        {
+            server.Command("halloween.enabled", on);
+            server.Command("halloween.murdererpopulation", on ? config.SpawnSettings.murdererPoluation : 0);
+            server.Command("halloween.scarecrowpopulation", on ? config.SpawnSettings.scarecrowPopulation : 0);
+
+            if (on) daysSinceSpawn = 0;
+        }
+
+        IEnumerator FindZombies()
+        {
+            var list = BaseNetworkable.serverEntities.entityList.Values;
+
+            for (int i = 0; i < list.Count; i++)
+                if (IsZombie(list[i]) && !serverZombies.Contains(list[i])) serverZombies.Add(list[i]);
+
+            yield break;
+        }
+
+        #endregion
+
+        #region -Chat Broadcast-
+
+        private void ChatBroadcast() //This is just an estimate but it is fine for now
+        {
+            float sqkm = (Terrain.activeTerrains[0].terrainData.size.x / 1000) * (Terrain.activeTerrains[0].terrainData.size.z / 1000);
+
+            float murderers = Halloween.murdererpopulation * sqkm;
+
+            float scarecrows = Halloween.scarecrowpopulation * sqkm;
+
+            foreach (BasePlayer player in BasePlayer.activePlayerList)
+                player.ChatMessage(config.BroadcastSettings.broadcastSeparate 
+                    ? lang.GetMessage("ChatBroadcastSeparate", this, player.UserIDString).Replace("{murdererCount}", Mathf.RoundToInt(murderers).ToString()).Replace("{scarecrowCount}", Mathf.RoundToInt(scarecrows).ToString()) 
+                    : lang.GetMessage("ChatBroadcast", this, player.UserIDString).Replace("{total}", (Mathf.RoundToInt(murderers + scarecrows).ToString())));
+        }
+
+        #endregion
+
+        #region -Destroying Zombies-
+
+        private void DoDestroy()
+        {
+            ServerMgr.Instance.StartCoroutine(DestroyZombies((numKilled) =>
+            {
+                ServerMgr.Instance.StartCoroutine(FindZombies());
+                timer.Once(0.5f, () => 
+                {
+                    if (serverZombies.Count > 0) DoDestroy();
+                    else Puts($"Destroyed {numKilled} zombies");
+                });
+            }));
+        }
+
+        IEnumerator DestroyZombies(Action<int> action)
+        {
+            int x = 0;
+            for (int i = 0; serverZombies.Count != 0; i++)
+            {
+                yield return new WaitForSeconds(0.15f);
+
+                BaseNetworkable networkable = null;
+
+                try
+                {
+                    networkable = serverZombies[0];
+                }
+                catch
+                {
+                    continue;
+                }
+
+                serverZombies.Remove(networkable);
+
+                if (networkable == null) continue;
+
+                if (config != null && config.DestroySettings.leaveCorpse)
+                {
+                    BaseCombatEntity entity = networkable as BaseCombatEntity;
+                    entity?.Hurt(entity.MaxHealth());
+                    x++;
+                }
+                else
+                {
+                    networkable.Kill();
+                    x++;
+                }
+            }
+
+            action(x);
+
+            yield break;
+        }
+
+        #endregion
 
         #region -Configuration-
 
-        private static Configuration _config;
-
         class Configuration
         {
-            [JsonProperty(PropertyName = "Murderer Population")]
-            public int murdererPopulation = 50;
-            
-            [JsonProperty(PropertyName = "Scarecrow Population")]
-            public int scarecrowPopulation = 50;
+            [JsonProperty(PropertyName = "Spawn Settings")]
+            public SpawnSettingsClass SpawnSettings = new SpawnSettingsClass();
 
-            [JsonProperty(PropertyName = "Kill Murderers")]
-            public bool killMurderers = true;
+            [JsonProperty(PropertyName = "Destroy Settings")]
+            public DestroySettingsClass DestroySettings = new DestroySettingsClass();
 
-            [JsonProperty(PropertyName = "Play sound")]
-            public bool playSound = false;
+            [JsonProperty(PropertyName = "Behaviour Settings")]
+            public BehaviourSettingsClass BehaviourSettings = new BehaviourSettingsClass();
 
-            [JsonProperty(PropertyName = "Broadcast Spawn amount")]
-            public bool doBroadcast = false;
+            [JsonProperty(PropertyName = "Broadcast Settings")]
+            public ChatSettings BroadcastSettings = new ChatSettings();
 
-            [JsonProperty(PropertyName = "Broadcast murderer and scarecrow count separately")]
-            public bool broadcastSeparate = false;
+            public class SpawnSettingsClass
+            {
+                [JsonProperty(PropertyName = "Murderer Population")]
+                public float murdererPoluation = 50;
 
-            [JsonProperty(PropertyName = "Slow destroy")]
-            public bool slowDestroy = false;
+                [JsonProperty(PropertyName = "Murderer Health")]
+                public float murdererHealth = 100f;
 
-            [JsonProperty(PropertyName = "Slow destroy time (seconds)")]
-            public float destroyTime = 0.15f;
+                [JsonProperty(PropertyName = "Scarecrow Population")]
+                public float scarecrowPopulation = 50;
+
+                [JsonProperty(PropertyName = "Scarecrow Health")]
+                public float scarecrowHealth = 200f;
+
+                [JsonProperty(PropertyName = "Reverse Spawn Timings")]
+                public bool reverseTimings = false;
+
+                [JsonProperty(PropertyName = "Chance Settings")]
+                public ChanceSetings Chance = new ChanceSetings();
+
+                public class ChanceSetings
+                {
+                    [JsonProperty(PropertyName = "Chance per cycle")]
+                    public float chance = 100f;
+
+                    [JsonProperty(PropertyName = "Days betewen spawn")]
+                    public int days = 0;
+                }
+            }
+
+            public class DestroySettingsClass
+            {
+                [JsonProperty(PropertyName = "Leave Corpse")]
+                public bool leaveCorpse = true;
+            }
+
+            public class BehaviourSettingsClass
+            {
+                [JsonProperty(PropertyName = "Count if shortname contains")]
+                public bool ifContains = false;
+
+                [JsonProperty(PropertyName = "Ignored entities")]
+                public List<string> ignored = new List<string>();
+            }
+
+            public class ChatSettings
+            {
+                [JsonProperty(PropertyName = "Broadcast spawn amount")]
+                public bool doBroadcast = false;
+
+                [JsonProperty(PropertyName = "Broadcast types separately")]
+                public bool broadcastSeparate = false;
+            }
         }
 
         protected override void LoadConfig()
@@ -56,133 +332,44 @@ namespace Oxide.Plugins
             base.LoadConfig();
             try
             {
-                _config = Config.ReadObject<Configuration>();
-                if (_config == null) throw new Exception();
+                config = Config.ReadObject<Configuration>();
+                if (config == null) throw new Exception();
                 SaveConfig();
             }
             catch
             {
-                PrintError("Your config contains an error or does not exist, using default values");
+                PrintError("Failed to load config, using default values");
                 LoadDefaultConfig();
             }
         }
 
-        protected override void SaveConfig() => Config.WriteObject(_config);
+        protected override void LoadDefaultConfig() => config = new Configuration
+        {
+            BehaviourSettings = new Configuration.BehaviourSettingsClass
+            {
+                ignored = new List<string>
+                {
+                    "scientist",
+                    "scientistjunkpile"
+                }
+            }
+        };
 
-        protected override void LoadDefaultConfig() => _config = new Configuration();
+        protected override void SaveConfig() => Config.WriteObject(config);
 
         #endregion
 
-        void OnServerInitialized(bool serverInit)
-        {
-            TOD_Sky.Instance.Components.Time.OnSunset += SpawnZombies;
-            TOD_Sky.Instance.Components.Time.OnSunrise += StartDestroyZombies;
-        }
+        #region -Localisation-
 
-        void Unload()
+        protected override void LoadDefaultMessages()
         {
-            TOD_Sky.Instance.Components.Time.OnSunset -= SpawnZombies;
-            TOD_Sky.Instance.Components.Time.OnSunrise -= StartDestroyZombies;
-
-            if (destroyTimer != null) destroyTimer.Destroy();
-        }
-
-        void SpawnZombies()
-        {
-            if (!halloweenActive)
+            lang.RegisterMessages(new Dictionary<string, string>
             {
-                Server.Command("halloween.enabled", true);
-                Server.Command("halloween.murdererpopulation", _config.murdererPopulation);
-                Server.Command("halloween.scarecrowpopulation", _config.scarecrowPopulation);
-                
-                halloweenActive = true;
-
-                if (_config.doBroadcast) timer.Once(15f, () => { ChatBroadcast(); if (_config.playSound) PlaySound(); });
-            }
+                ["ChatBroadcast"] = "[Night Zombies] Spawned {murdererCount} murderers | Spawned {scarecrowCount} scarecrows",
+                ["ChatBroadcastSeparate"] = "[Night Zombies] Spawned {total} zombies"
+            }, this);
         }
 
-        void StartDestroyZombies()
-        {
-            if (halloweenActive)
-            {
-                Server.Command("halloween.enabled", false);
-                Server.Command("halloween.murdererpopulation", 0);
-                Server.Command("halloween.scarecrowpopulation", 0);
-
-                timer.Once(5f, () => ServerMgr.Instance.StartCoroutine(DestroyZombies()));
-            }
-        }
-
-        IEnumerator DestroyZombies()
-        {
-            if (_config.slowDestroy)
-            {
-                destroyTimer = timer.Every(_config.destroyTime, () =>
-                {
-                    if (BaseNetworkable.serverEntities.Any(x => (x is NPCMurderer || x is HTNPlayer) && !(x is Scientist || x is ScientistNPC || x.ShortPrefabName.Contains("scientist"))))
-                    {
-                        BaseCombatEntity ent = BaseNetworkable.serverEntities.FirstOrDefault(x => x is NPCMurderer || x is HTNPlayer) as BaseCombatEntity;
-
-                        if (ent == null) destroyTimer.Destroy();
-
-                        if (_config.killMurderers) ent.Hurt(ent.MaxHealth());
-                        else
-                        {
-                            ent.Kill();
-                            UnityEngine.Object.Destroy(ent);
-                        }
-                    }
-                    else destroyTimer.Destroy();
-                });
-            }
-            else
-            {
-                while (BaseNetworkable.serverEntities.Any(x => (x is NPCMurderer || x is HTNPlayer) && !(x is Scientist || x is ScientistNPC || x.ShortPrefabName.Contains("scientist"))))
-                {
-                    foreach (BaseNetworkable networkable in BaseNetworkable.serverEntities)
-                    {
-                        if ((networkable is HTNPlayer || networkable is NPCMurderer) && !(networkable is Scientist || networkable is ScientistNPC || networkable.ShortPrefabName.Contains("scientist")))
-                        {   
-                            BaseCombatEntity ent = networkable as BaseCombatEntity;
-
-                            if (_config.killMurderers) ent.Hurt(ent.MaxHealth());
-                            else
-                            {
-                                ent.Kill();
-                                UnityEngine.Object.Destroy(ent);
-                            }
-                        }
-                    }
-                    yield return null;
-                }
-            }
-
-            halloweenActive = false;
-
-            yield break;
-        }
-
-        private void ChatBroadcast()
-        {
-            float sqkm = (Terrain.activeTerrains[0].terrainData.size.x / 1000) * (Terrain.activeTerrains[0].terrainData.size.z / 1000);
-
-            float murderers = _config.murdererPopulation * sqkm;
-
-            var scarecrows = _config.scarecrowPopulation * sqkm;
-
-            string text = _config.broadcastSeparate ? $"[Night Zombies] Spawned {Mathf.RoundToInt(murderers)} murderers | Spawned {Mathf.RoundToInt(scarecrows)} scarecrows" : $"[Night Zombies] Spawned {Mathf.RoundToInt(murderers + scarecrows)} zombies";
-
-            PrintToChat(text);
-        }
-
-        [ChatCommand("test")]
-        private void PlaySound()
-        {
-            foreach (BasePlayer player in Player.Players)
-            {
-                Effect effect = new Effect("assets/prefabs/misc/halloween/spookyspeaker/sound/spookysounds.asset", player, 0, Vector3.zero, Vector3.forward);
-                EffectNetwork.Send(effect, player.net.connection);
-            }
-        }
+        #endregion
     }
 }
